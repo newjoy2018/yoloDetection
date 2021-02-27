b04665aff94b1916c6b756fa5c45bb04dbea5bda
Newjoy <38489219+newjoy2018@users.noreply.github.com>
2021-02-27 17:43:03 +0100

    Create lossCurveDrawing.py

---
+++ logAnalysis/lossCurveDrawing.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+lines = 10000
+result = pd.read_csv('yolov4-tinyLog_formatted.csv', error_bad_lines=False, names=['loss', 'avg', 'rate', 'seconds', 'images'])
+# skiprows=[x for x in range(lines) if ((x%10 != 9))]
+
+result['loss']=result['loss'].str.split(' ').str.get(1)
+result['loss']=pd.to_numeric(result['loss'])
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+x = np.arange(0,10000,1)
+ax.scatter(x,result['loss'].values,s=1)
+# ax.legend(loc='best')
+ax.set_title('Loss Curve')
+ax.set_xlabel('Itrations')
+ax.set_ylabel('Loss')
+fig.savefig('Loss-Itrations.png', dpi=3600)
